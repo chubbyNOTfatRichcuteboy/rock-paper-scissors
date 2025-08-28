2dad4f37d212e9b26789e90fda6b13771507f104
Cute Rich Boy <218043178+chubbyNOTfatRichcuteboy@users.noreply.github.com>
2025-08-27 22:12:35 -0700

    complete js scripting, html/css partway
    Complete JS, barebones html and css
    Need to work on bettering the UI

--- code.js
+++ code.js
@@ -12,42 +12,73 @@ function getComputerChoice() {
     return(choice);
 }
 
-function getHumanChoice() {
-    let input = prompt("Choose either rock, paper, or scissors: ");
-    return(input);
-}
-
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
+    const div = document.querySelector("#gameplay");
+
     function playRound(humanChoice, computerChoice) {
-        hc = humanChoice.toLowerCase()
+        function check() {
+            if (humanScore === 5 || computerScore === 5) {
+                const win = document.querySelector('#win');
+                div.remove();
+                win.textContent = `Game over! Final score 
+                    ${humanScore}:${computerScore}`;
+        }}
+
+        hc = humanChoice.toLowerCase();
         if (humanChoice.toLowerCase() === computerChoice) {
-            console.log("Tie");
+            return("tie");
         } else {
             if (hc === "rock" && computerChoice === "scissors") {
                 humanScore++;
-                console.log("You win");
+                check();
+                return "win";
             } else if (hc === "paper" && computerChoice === "rock") {
                 humanScore++;
-                console.log("You win");
+                check();
+                return "win";
             } else if (hc === "scissors" && computerChoice === "paper") {
                 humanScore++;
-                console.log("You win");
+                check();
+                return "win";
             } else {
                 computerScore++;
-                console.log("You lose");
+                check();
+                return("lose");
             }
         }
+
     }
 
-    for (let i = 0; i < 5; i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
+    const choices = document.querySelectorAll(".choice");
+    const pscore = document.querySelector("#pscore");
+    const cscore = document.querySelector("#cscore");
+    const uC = document.querySelector("#userChoice");
+    const cC = document.querySelector("#computerChoice");
+    const roundWin = document.querySelector("#roundWin");
+
+    choices.forEach((choice) => {
+        choice.addEventListener("click", () => {
+            console.log(choice.id);
+            let humanChoice = choice.id;
+            let computerChoice = getComputerChoice();
+            let roundResult = playRound(humanChoice, computerChoice);
+
+            roundWin.textContent = `You ${roundResult}`;
+            uC.textContent = `You chose ${humanChoice}`;
+            cC.textContent = `The computer chose ${computerChoice}`;
+            pscore.textContent = `Your score: ${humanScore}`;
+            cscore.textContent = `Computer score: ${computerScore}`;
+        })
+    })
 }
 
+playGame();
 
-playGame()
+
+
+
+
+
